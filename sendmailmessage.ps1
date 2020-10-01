f67ef84077b1b66470edc8e8fa552b01e093e0e9
Matt Staude <50743403+mattstaude@users.noreply.github.com>
2020-10-01 15:14:15 -0500

    Create sendmailmessage.ps1
    The script

---
+++ sendmailmessage.ps1
@@ -0,0 +1,17 @@
+# Created By Matt Staude
+# The purpose of this script is to send an email with all files in a specified folder as an attachment.  
+# This part sends the email
+# Please set up a connector if you wish to use  this script without requireing login.
+$parameters = @{
+    from        = "from@mydomain.com"
+    to          = "to@yourdomain.com"
+    subject     = "***Attachment(s) Enclosed***"
+    body = "Please inspect the included attachment(s).  This is an automatically generated email."
+    smtpserver  = "your.smtp.server.net"
+    attachments = (get-childitem "C:\source\location").fullname
+    
+}
+#Sends the message
+Send-MailMessage @parameters
+# This part archives the files into a destination folder AFTER the email sends.
+Get-ChildItem -Path 'C:\source\location' -Recurse | Move-Item -force -Destination 'C:\destination\location\'
